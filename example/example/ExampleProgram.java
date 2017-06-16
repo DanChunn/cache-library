23a3d6f123f5e50c9227eb1e82725a93717b6e8a
danc <danchunn@users.noreply.github.com>
2017-06-16 14:08:42 -0700

    added jar, docs, readme, uml

--- example/example/ExampleProgram.java
+++ example/example/ExampleProgram.java
@@ -1,130 +1,19 @@
 package example;
 
-import java.util.Scanner;
-
 import policy.*;
 import sac.SetAssociativeCache;
 
 
 /** 
- * An example program demonstrating a cache that takes Integer, Character pairs.
+ * An example program.
  * */
 public class ExampleProgram {
     public static void main(String[] args) {
     	
-    	Policy<Integer> policy = null;
-    	
-    	System.out.println("Set Associative Cache");
-    	System.out.println("-- by Daniel Chunn --");
-    	System.out.println("------Example--------");
-    	System.out.println();
-    	Scanner sc = new Scanner(System.in);  
-    	System.out.println("Available Commands. Enter a number to execute command.");
-    	System.out.println("1) Create cache with LRU policy.");
-    	System.out.println("2) Create cache with MRU policy.");
-    	System.out.println("3) Exit.");
-    	
-    	int n = sc.nextInt(); 
-    	while(n <= 0 || n > 3){
-    		System.out.println("Must enter a number above.");
-    		n = sc.nextInt(); 
-    	}
-    	
-    	if(n == 1){
-    		policy = new PolicyLRU<>();
-    		System.out.println("LRU Policy created.");
-    	} else if (n == 2){
-    		policy = new PolicyMRU<>();
-    		System.out.println("MRU Policy created.");
-    	} else if (n == 3){
-    		exitProgram(sc);
-    	} 
-    	
-    	System.out.println("Enter the amount of entries per set:");
-
-    	n = sc.nextInt(); 
-    	while(n <= 0 || n > Integer.MAX_VALUE){
-    		checkBounds(n);
-    		n = sc.nextInt(); 
-    	}
-    	int blocks = n;
-    	
-    	System.out.println("Enter the amount of sets in the cache");
-    	
-    	n = sc.nextInt(); 
-    	while(n <= 0 || n > Integer.MAX_VALUE){
-    		checkBounds(n);
-    		n = sc.nextInt(); 
-    	}
-    	int sets = n;
-    	
-    	
-    	SetAssociativeCache<Integer, Character> sac = new SetAssociativeCache<>(blocks,sets,policy);
-    	System.out.println("The cache has been created. It has sets of size " + blocks + " and has " + sets + " sets.");
-    	System.out.println("*** For this example, the keys are Integers and the values are Characters. ***");
-    	System.out.println();
-    	System.out.println("Available Commands. Enter a string to execute command.");
-    	System.out.println("get INTEGER            | returns a character value      | ex. get 5");
-    	System.out.println("put INTEGER CHARACTER  | places the pair into the cache | ex. put 5 A");
-    	System.out.println("print                  | prints the cache items         | ex. print");
-    	System.out.println("exit                   | exits the program              | ex. exit");
-    	
-    	String s = "";
-    	
-    	while(s != "exit"){
-    		s = sc.nextLine();
-    		String[] arr = s.split(" ");
-    		
-    		if(arr == null || arr.length == 0){
-    			printError();
-    			continue;
-    		}
-    		
-    		try{
-    			
-        		if(arr[0].equals("get") ){
-
-        			Integer k = Integer.parseInt(arr[1]);
-        			
-        			if(sac.get(k) == null){
-        				System.out.println("No value found.");
-        			} else{
-        				System.out.println(">> "+sac.get(k));
-        			}
-        		} 
-        		else if (arr[0].equals("put")){
-        			
-        			Integer k = Integer.parseInt(arr[1]);
-        			Character v = arr[2].charAt(0);
-        			sac.put(k, v);
-        			System.out.println(("("+ k + " , " + v + ") placed!"));
-        		} 
-        		
-        		else if (arr[0].equals("print")){
-        			
-        			sac.print();
-        		} 
-        		
-        		else if (arr[0].equals("exit")){
-        			
-        			exitProgram(sc);	
-        		} 
-    		} catch (Exception e){
-    			printError();
-    			continue;
-    		}
-
-    	}
-
-    	exitProgram(sc);
-    	
-		
     	/**
     	 * Some more examples listed below...
     	 */
-    	
-    	/*
-    	//A policy instance is created.
+    	//An MRU policy instance is created.
 		Policy<Integer> policy1 = new PolicyMRU<>();
 		
 		//A cache is created with 3 sets of size 2 each.
@@ -133,14 +22,16 @@ public class ExampleProgram {
 		sac1.put(0, 'a');
 		sac1.put(1, 'b');
 		sac1.put(2, 'c');
-		sac1.put(3, 'd');
-		sac1.put(4, 'e');
-		sac1.put(5, 'f');
+		sac1.put(3, 'd'); 
+		sac1.put(4, 'e'); 
+		sac1.put(5, 'f'); 
 		sac1.put(6, 'g'); //full set, replaces (3,'d') 
 		sac1.put(1, 'z'); //overwrites (1, 'b') to (1,'z') and is now MRU within set
-		sac1.put(7, 'y'); //full set, replaces (1, 'b')
+		sac1.put(7, 'y'); //full set, replaces (1, 'z')
 		sac1.get(2);      //(2,'c') is now MRU within set
-		sac1.put(8, 'w'); //full set, replaces (2,'a')
+		sac1.put(8, 'w'); //full set, replaces (2,'c')
+		sac1.get(1);      //returns null   
+		sac1.get(2);      //returns null
 		
 		System.out.println();
 		for(int i = 0; i <= 8; i++){
@@ -150,7 +41,7 @@ public class ExampleProgram {
 		sac1.print();
 
 
-    	//A policy instance is created.
+    	//An LRU policy instance is created.
 		Policy<Integer> policy2 = new PolicyLRU<>();
 		
 		//A cache is created with 1 sets of size 4 each.
@@ -171,24 +62,8 @@ public class ExampleProgram {
 		System.out.println();
 		for(int i = 0; i <= 7; i++){
 			System.out.println(i + " : " + sac2.get(i));
-		}*/
-    }
-    
-    private static void printError(){
-    	System.out.println("Improper input. Try again.");
-    }
-    
-    private static void checkBounds(int n){
-		if(n <= 0){
-			System.out.println("Enter a number above 0.");
-		} else{
-			System.out.println("That value is too large.");
 		}
-    }
-    
-    private static void exitProgram(Scanner sc){
-    	System.out.println("Exiting program...");
-		sc.close();
-		System.exit(0);
+		
+		sac2.print();
     }
 }
